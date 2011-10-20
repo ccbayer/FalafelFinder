3d898cd83b26fadc863ab3f967730a4ddad6342d
ccbayer@gmail.com <ccbayer@gmail.com>
2011-10-19 23:07:59 -0500

    added new views and styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
+  
   belongs_to :user
   has_many :tags
   has_many :comments
+  
+  #presence
+  validates_presence_of :title, :postcontent, :user_id, :type => true
+  
+  
 end
